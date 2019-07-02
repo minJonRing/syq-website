de2f4e7c45ec98d655ce1aa1a627bb2b73e9d049
jonrong <qiaorong@qiaorongdeMacBook-Pro.local>
2019-07-02 15:00:22 +0800

    1

--- routes/wx.js
+++ routes/wx.js
@@ -3,10 +3,18 @@ const request = require("request");
 
 router.get("/app/wx/token",async(ctx, next)=>{
     
-    const data = await new Promise((resolve,reject)=>{
+    const token = await new Promise((resolve,reject)=>{
         request("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx2df0a9fe18f3cb02&secret=fb964c9eeaa0895370ddeef2e59fb9d7",async(err,res,body)=>{
             if(!err && res.statusCode == 200){
-                console.log(res,body);
+                let token = res.body['access_token']
+                resolve(token)
+            }
+        })
+    })
+
+    const data = new Promise((resolve,reject)=>{
+        request("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token="+token+"&type=jsapi",async(err,res,body)=>{
+            if(!err){
                 resolve(res)
             }
         })
