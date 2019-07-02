6d9a44d0ef2503dce882ea47dafb3ce07838a6d3
minJonRing <1014080407tqr@gmail.com>
2019-07-02 19:24:42 +0800

    2

--- routes/wx.js
+++ routes/wx.js
@@ -5,7 +5,7 @@ const request = require("request");
 let token = "",time = Date.now();
 router.get("/app/wx/token",async(ctx, next)=>{
     let now = Date.now();
-    // if(!token || (now - time) > 7000*1000){
+    if(!token || (now - time) > 7000*1000){
         token = await new Promise((resolve,reject)=>{
             request("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx2df0a9fe18f3cb02&secret=fb964c9eeaa0895370ddeef2e59fb9d7",async(err,res,body)=>{
                 if(!err && res.statusCode == 200){
@@ -14,18 +14,19 @@ router.get("/app/wx/token",async(ctx, next)=>{
                 }
             })
         })
-    // }
+    }
 
     const data = await new Promise((resolve,reject)=>{
         request("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token="+token+"&type=jsapi",async(err,res,body)=>{
             if(!err){
-                resolve(res)
+                let icket = JSON.parse(res.body)['ticket'];
+                resolve(icket)
             }else{
                 reject(err)
             }
         })
     })
-    ctx.body = {msg:token,data:data,url:"https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token="+token+"&type=jsapi"};
+    ctx.body = {msg:token,data:data};
 })
 
 module.exports = router;
