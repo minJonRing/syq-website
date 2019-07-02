454eed65cd9c48680c9b2838d2012b9ee10e9145
jonrong <qiaorong@qiaorongdeMacBook-Pro.local>
2019-07-02 15:05:23 +0800

    1

--- routes/wx.js
+++ routes/wx.js
@@ -1,16 +1,20 @@
 const router = require('koa-router')()
 const request = require("request");
 
+
+let token,time = Date.now();
 router.get("/app/wx/token",async(ctx, next)=>{
-    
-    const token = await new Promise((resolve,reject)=>{
-        request("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx2df0a9fe18f3cb02&secret=fb964c9eeaa0895370ddeef2e59fb9d7",async(err,res,body)=>{
-            if(!err && res.statusCode == 200){
-                let token = res.body['access_token']
-                resolve(token)
-            }
+    let now = Date.now();
+    if(!token || (now - time) > 7000*1000){
+        token = await new Promise((resolve,reject)=>{
+            request("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx2df0a9fe18f3cb02&secret=fb964c9eeaa0895370ddeef2e59fb9d7",async(err,res,body)=>{
+                if(!err && res.statusCode == 200){
+                    let token = res.body['access_token']
+                    resolve(token)
+                }
+            })
         })
-    })
+    }
 
     const data = await new Promise((resolve,reject)=>{
         request("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token="+token+"&type=jsapi",async(err,res,body)=>{
@@ -19,7 +23,7 @@ router.get("/app/wx/token",async(ctx, next)=>{
             }
         })
     })
-    ctx.body = {data:data};
+    ctx.body = {token:token,data:data};
 })
 
 module.exports = router;
